65ee37f4cf555924746f365de99bcd99da5a9a7a
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2025-04-08 13:45:21 +0200

    Add base_url support for deployment

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 ## Deployment settings
-domain = "jekyll-theme-emojification.michaelnordmeyer.com"
+base_url = `yq '.url += .baseurl' _config.yml | grep -E '^url:' | cut -d' ' -f2 | cut -c9- | tr -d '\n'`
+if base_url == ""
+  puts "Error: base_url cannot be determined"
+  exit!(1)
+end
 ssh_domain = "michaelnordmeyer.com"
 ssh_port = 1111
 ssh_user = "root"
-ssh_path = "/var/www/#{domain}/"
+ssh_path = "/var/www/#{base_url}/"
 
 task :default => ["build"]
 
@@ -17,20 +21,20 @@ end
 
 desc "Builds the robots.txt"
 task :robots do
-  puts "==> Building #{domain} robots.txt..."
-  sh "printf 'Sitemap: https://#{domain}/sitemap.xml\\n\\n' > robots.txt"
+  puts "==> Building #{base_url} robots.txt..."
+  sh "printf 'Sitemap: https://#{base_url}/sitemap.xml\\n\\n' > robots.txt"
   sh "cat ../../../michaelnordmeyer.com/robots.txt >> robots.txt"
 end
 
 desc "Builds the feed icon"
 task :feedicon do
-  puts "==> Building #{domain} feed icon..."
+  puts "==> Building #{base_url} feed icon..."
   sh "cp _site/assets/icons/$(yq '.favicon' < _config.yml) _site/assets/icons/$(yq '.feed.icon' < _config.yml)"
 end
 
 desc "Beautifies kramdown output"
 task :beautify do
-  puts "==> Beautifying #{domain} kramdown output..."
+  puts "==> Beautifying #{base_url} kramdown output..."
   sh "for file in _site/*.html _site/**/*.html; do sed -i '' -E 's,<((br|hr|img|link|meta).*) />,<\\1>,g' ${file}; done"
   sh "for file in _site/*.html _site/**/*.html; do sed -i '' -E 's/ class=\"footnotes?\"//g' ${file}; done"
   sh "for file in _site/*.html _site/**/*.html; do sed -i '' 's/ class=\"reversefootnote\"//g' ${file}; done"
@@ -39,7 +43,7 @@ end
 desc "Builds the site for deployment"
 task :build do
   Rake::Task[:robots].invoke
-  puts "==> Building #{domain}..."
+  puts "==> Building #{base_url}..."
   sh "JEKYLL_ENV=\"production\" bundle exec jekyll build"
   Rake::Task[:feedicon].invoke
   Rake::Task[:beautify].invoke
@@ -48,13 +52,13 @@ end
 desc "Serves the site locally"
 task :serve do
   Rake::Task[:robots].invoke
-  puts "==> Building and serving #{domain} locally..."
+  puts "==> Building and serving #{base_url} locally..."
   sh "bundle exec jekyll serve"
 end
 
 desc "Syncs the content of ./_site to the server via rsync"
 task :rsync do
-  puts "==> Rsyncing #{domain}'s content to SSH host #{ssh_domain}"
+  puts "==> Rsyncing #{base_url}'s content to SSH host #{ssh_domain}"
   sh "rsync -e 'ssh -p #{ssh_port}' -vcrlptDShP --delete --rsync-path 'sudo -u root rsync' --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r \
     --exclude=.DS_Store \
     --exclude=._* \
@@ -66,25 +70,25 @@ end
 
 desc "Copies robots.txt to the server via scp"
 task :scprobots do
-  puts "==> Scp’ing #{domain} robots.txt to SSH host #{ssh_domain}"
+  puts "==> Scp’ing #{base_url} robots.txt to SSH host #{ssh_domain}"
   sh "scp -P #{ssh_port} robots.txt #{ssh_user}@#{ssh_domain}:#{ssh_path}"
 end
 
 desc "Gzips the site via SSH"
 task :gzip do
-  puts "==> Gzip'ing #{domain} via SSH..."
+  puts "==> Gzip'ing #{base_url} via SSH..."
   sh "ssh -p #{ssh_port} #{ssh_user}@#{ssh_domain} 'for file in $(find #{ssh_path} -type f -name \"*.html\" -o -name \"*.css\" -o -name \"*.css.map\" -o -name \"*.js\" -o -name \"*.svg\" -o -name \"*.xml\" -o -name \"*.xsl\" -o -name \"*.xslt\" -o -name \"*.json\" -o -name \"*.txt\"); do printf . && gzip -kf \"${file}\"; done; echo'"
 end
 
 desc "Gzips robots.txt via SSH"
 task :gziprobots do
-  puts "==> Gzip'ing #{domain} robots.txt via SSH..."
+  puts "==> Gzip'ing #{base_url} robots.txt via SSH..."
   sh "ssh -p #{ssh_port} #{ssh_user}@#{ssh_domain} 'gzip -kf #{ssh_path}robots.txt'"
 end
 
 desc "Builds and deploys the site"
 task :deploy do
-  puts "==> Building and deploying #{domain}..."
+  puts "==> Building and deploying #{base_url}..."
   Rake::Task[:build].invoke
   Rake::Task[:rsync].invoke
   Rake::Task[:gzip].invoke
@@ -93,7 +97,7 @@ end
 
 desc "Builds and deploys the robots.txt"
 task :deployrobots do
-  puts "==> Building and deploying #{domain} robots.txt..."
+  puts "==> Building and deploying #{base_url} robots.txt..."
   Rake::Task[:robots].invoke
   Rake::Task[:scprobots].invoke
   Rake::Task[:gziprobots].invoke
@@ -101,6 +105,6 @@ end
 
 desc "Cleans the source dir"
 task :clean do
-  puts "==> Cleaning #{domain}..."
+  puts "==> Cleaning #{base_url}..."
   sh "bundle exec jekyll clean"
 end
